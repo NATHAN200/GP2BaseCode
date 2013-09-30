ea81be0183990f95fe10fe4f92d9cb3955367ac6
Nicholas <NATHAN200@caledonian.ac.uk>
2013-09-30 20:27:23 +0100

    Renderer.h

---
+++ GP2BaseCode/Renderer/Renderer.h
@@ -0,0 +1,17 @@
+//header guard, so this is only compiled once
+#pragma once
+
+
+class IRenderer
+{
+public:
+	//virtual deconstructor, so this class can be overridden
+	virtual ~IRenderer(){};
+	//virtual function, can be overridden
+	virtual bool init(void *pWindowHandle,bool fullScreen)=0;
+	//virtual function, can be overridden
+	virtual void clear(float r,float g,float b,float a)=0;
+	//virtual function, can be overridden
+	virtual void present()=0;
+};
+
