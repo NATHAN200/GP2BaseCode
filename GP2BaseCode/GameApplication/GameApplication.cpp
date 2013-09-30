1a54201819f1069cf6331abc9702f76809fc0140
Nicholas <NATHAN200@caledonian.ac.uk>
2013-09-30 20:15:26 +0100

    Excercise 1 complete

--- GP2BaseCode/GameApplication/GameApplication.cpp
+++ GP2BaseCode/GameApplication/GameApplication.cpp
@@ -20,11 +20,106 @@ CGameApplication::CGameApplication(void)
 }
 
 //Deconstructor
-CGameApplication::~CGameAppliction(void)
+CGameApplication::~CGameApplication(void)
 {
 	if(m_pWindow)
 	{
 		delete m_pWindow;
 		m_pWindow=NULL;
 	}
+}
+
+//Init
+//This intialises all subsystems
+bool CGameApplication::init()
+{
+	if(!parseConfigFile())
+		return false;
+	if(!initWindow())
+		return false;
+	if(!initGraphics())
+		return false;
+	if(!initInput())
+		return false;
+	return true;
+}
+
+
+
+//called to parse the config file
+bool CGameApplication::parseConfigFile()
+{
+
+
+	return true;
+}
+
+
+//initInput - Initialises the input
+bool CGameApplication::initInput()
+{
+	return true;
+}
+
+
+//initPhysics - Initialises the physics system
+bool CGameApplication::initPhysics()
+{
+	return true;
+}
+
+
+//initGraphics - initialise the graphics subsystem
+bool CGameApplication::initGraphics()
+{
+	return true;
+}
+
+
+//initWindow - initialise the window
+bool CGameApplication::initWindow()
+{
+	//Create a Win32 Window
+	m_pWindow=new CWin32Window();
+	m_pWindow->init(m_GameOptionDesc.gameName,m_GameOptionDesc.width,m_GameOptionDesc.height,m_GameOptionDesc.fullscreen);
+	return true;
+}
+
+
+//called to init the game
+bool CGameApplication::initGame()
+{
+
+
+	return true;
+}
+
+
+//Called to put the game in a loop(aka game loop)
+void CGameApplication::run()
+{
+	//while the window is not closed
+	while(m_pWindow->running())
+	{
+		//check for all windows messages
+		m_pWindow->checkForWindowMessages();
+		//update
+		update();
+		//render
+		render();
+	}
+}
+
+
+//Render, called to draw one frame of the game
+void CGameApplication::render()
+{
+
+
+}
+
+
+//Update, called to update the game
+void CGameApplication::update()
+{
 }
--- GP2BaseCode/GameApplication/GameApplication.h
+++ GP2BaseCode/GameApplication/GameApplication.h
@@ -6,3 +6,47 @@
 using namespace std;
 
 //Forward decleration of our windows interfce
+class IWindow;
+
+//Structure for holding GameOptions, this will be loaded from config files
+struct GameOptionsDesc
+{
+
+	wstring gameName;
+	int width;
+	int height;
+	bool fullscreen;
+};
+
+//we have this here so we don't need to prefic all
+// standard library types with std::
+using namespace std;
+
+class CGameApplication
+{
+public:
+	CGameApplication(void);
+	//virtual deconstructor, so this class can be overridden
+	virtual ~CGameApplication(void);
+	//virtual function, can be overridden
+	virtual bool init();
+	void run();
+	//virtual function, can be overridden
+	virtual void render();
+	//virtual function, can be overridden
+	virtual void update();
+
+private:
+	bool parseConfigFile();
+	bool initInput();
+	bool initGame();
+	bool initGraphics();
+	bool initPhysics();
+
+	bool initWindow();
+
+private:
+	IWindow * m_pWindow;
+	GameOptionsDesc m_GameOptionDesc;
+	wstring m_ConfigFileName;
+};
